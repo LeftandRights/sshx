1fd60d2d4be570480da8f5b84c804319cc539a56
LeftandRight <83283220+LeftandRights@users.noreply.github.com>
2025-04-14 03:04:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import subprocess
-
 from flask import redirect
 from flask import Flask; app = Flask(
   __name__
 )
 
-proc = subprocess.Popen(["sshx"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+proc = subprocess.Popen(["sshx"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text = True)
 link = None
 
 for line in proc.stdout:
   if "Link" not in line:
     continue
 
-  link = line[line.index("https://"):]
+  link = [   _ for _ in line[line.index("https://"):]   ]
+  link = link[:link.index('\x1b')]
+  link = "".join(link)
+
   print("🔗 Captured SSHX Link:", link)
   break
 
 @app.route("/")
 def index() -> None:
-  return redirect(link.strip())
+  return redirect(link)
 
 if __name__ == "__main__":
   app.run(host = "0.0.0.0", port = 5000)
