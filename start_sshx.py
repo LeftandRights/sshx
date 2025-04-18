28135573dca88dd3425f92c15a97fe0437377c5f
LeftandRight <83283220+LeftandRights@users.noreply.github.com>
2025-04-18 16:43:28 +0800

    Update start_sshx.py

--- start_sshx.py
+++ start_sshx.py
@@ -1,8 +1,4 @@
 import subprocess
-from flask import redirect
-from flask import Flask; app = Flask(
-  __name__
-)
 
 proc = subprocess.Popen(["sshx"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text = True)
 link = None
@@ -15,12 +11,4 @@ for line in proc.stdout:
   link = link[:link.index('\x1b')]
   link = "".join(link)
 
-  print("🔗 Captured SSHX Link:", link)
-  break
-
-@app.route("/")
-def index() -> None:
-  return redirect(link)
-
-if __name__ == "__main__":
-  app.run(host = "0.0.0.0", port = 5000)
+  print(link)
